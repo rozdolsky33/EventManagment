aa79b1a60e6d2ecb1d01f8c6c779ef42f53ac9e7
Volodymyr <rozdolskyvolodymyr@gmail.com>
2019-04-24 17:10:25 -0400

    adding PagingAndSorting

--- src/main/java/com/eventmanagment/arwest/repo/VenueRepository.java
+++ src/main/java/com/eventmanagment/arwest/repo/VenueRepository.java
@@ -1,6 +1,7 @@
 package com.eventmanagment.arwest.repo;
 
 import com.eventmanagment.arwest.entities.Venue;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface VenueRepository extends PagingAndSortingRepository<Venue, Long> {
